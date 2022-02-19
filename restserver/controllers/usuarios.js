4c3c4b54ddaa676db478a1e755e212f2df1a5b1a
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-02-19 14:49:15 -0600

    restServer básico

---
+++ restserver/controllers/usuarios.js
@@ -0,0 +1,51 @@
+const { response, request } = require("express");
+
+const usuariosGet = (req = request, res = response) => {
+  const { q, nombre = "No name", apiKey, page = 1, limit } = req.query;
+  res.json({
+    msg: "get API - controlador",
+    q,
+    nombre,
+    apiKey,
+    page,
+    limit,
+  });
+};
+
+const usuariosPost = (req, res = response) => {
+  const { nombre, edad } = req.body;
+
+  res.json({
+    msg: "post API - controlador",
+    nombre,
+    edad,
+  });
+};
+
+const usuariosPut = (req, res = response) => {
+  const id = req.params.id;
+  res.json({
+    msg: "put API - controlador",
+    id,
+  });
+};
+
+const usuariosDelete = (req, res = response) => {
+  res.json({
+    msg: "delete API - controlador",
+  });
+};
+
+const usuariosPatch = (req, res = response) => {
+  res.json({
+    msg: "patch API - controlador",
+  });
+};
+
+module.exports = {
+  usuariosGet,
+  usuariosPost,
+  usuariosPut,
+  usuariosDelete,
+  usuariosPatch,
+};
