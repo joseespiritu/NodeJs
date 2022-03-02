80bbd4edebbf01cce3f906837f07c6c02471003f
joseespiritu <joseespiritu89@aragon.unam.mx>
2022-03-01 21:25:52 -0600

    Subida de archivos

--- restserver/middlewares/index.js
+++ restserver/middlewares/index.js
@@ -1,8 +1,10 @@
 const validarCampos = require("../middlewares/validar-campos");
 const validarJWT = require("../middlewares/validar-jwt");
 const validaRoles = require("../middlewares/validar-roles");
+const validarArchivo = require("../middlewares/validar-archivo");
 
 module.exports = {
+  ...validarArchivo,
   ...validarCampos,
   ...validarJWT,
   ...validaRoles,
--- restserver/models/server.js
+++ restserver/models/server.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const cors = require("cors");
+const fileUpload = require("express-fileupload");
 const { dbConnection } = require("../database/config");
 
 class Server {
@@ -12,6 +13,7 @@ class Server {
       categorias: "/api/categorias",
       productos: "/api/productos",
       usuarios: "/api/usuarios",
+      uploads: "/api/uploads",
     };
 
     // Conectar a base de datos
@@ -37,6 +39,15 @@ class Server {
 
     //   Directorio publico
     this.app.use(express.static("public"));
+
+    // File Upload - Carga de archivos
+    this.app.use(
+      fileUpload({
+        useTempFiles: true,
+        tempFileDir: "/tmp/",
+        createParentPath: true,
+      })
+    );
   }
 
   routes() {
@@ -45,6 +56,7 @@ class Server {
     this.app.use(this.paths.categorias, require("../routes/categorias"));
     this.app.use(this.paths.productos, require("../routes/productos"));
     this.app.use(this.paths.usuarios, require("../routes/usuarios"));
+    this.app.use(this.paths.uploads, require("../routes/uploads"));
   }
 
   listen() {
