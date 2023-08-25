6454394cb47f2f68d8a720faf54f2312f9bcc787
joseespiritu <joseespiritu89@aragon.unam.mx>
2023-08-25 07:04:00 -0600

    JWT Protected Route

--- bootcamp-master/natours/controllers/errorController.js
+++ bootcamp-master/natours/controllers/errorController.js
@@ -5,6 +5,10 @@ const handleCastErrorDB = (err) => {
     return new AppError(message, 400);
 };
 
+const handleJWTError = () => new AppError('Invalid token. Please log in again!', 401);
+
+const handleJWTExpiredError = () => new AppError('Your token has expired. Please log in again', 401);
+
 const handleDuplicateFieldsDB = (err) => {
     const value = err.errmsg.match(/(["'])(\\?.)*?\1/)[0];
     const message = `Duplicate field value: ${value}. Please use another value!`;
@@ -58,6 +62,8 @@ module.exports = (err, req, res, next) => {
         if (err.name === 'CastError') error = handleCastErrorDB(err);
         if (err.code === 11000) error = handleDuplicateFieldsDB(err);
         if (err.name === 'ValidationError') error = handleValidationErrorDB(err);
+        if (err.name === 'JsonWebTokenError') error = handleJWTError(err);
+        if (err.name === 'TokenExpiredError') error = handleJWTExpiredError(err);
 
         sendErrorProd(error, res);
     }
